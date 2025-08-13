093da0e4b29612a7b829a4126997e66a110eea60
halityrd <halityrd@hotmail.com>
2025-08-14 02:07:18 +0300

    Splitting Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,6 @@
+import 'package:dice_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
-      ),
-    );
-  }
 }
