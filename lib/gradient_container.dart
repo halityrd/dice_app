9c5bc1c260618754aa6325831201554b616df60a
halityrd <halityrd@hotmail.com>
2025-08-14 02:47:16 +0300

    Variables and Properties

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,7 +1,12 @@
 import 'package:dice_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -11,11 +16,11 @@ class GradientContainer extends StatelessWidget {
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(child: StyledText('Hello World')),
     );
   }
 }
