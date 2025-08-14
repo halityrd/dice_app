20fb00db40f76a4c9996aa804bbc2c0921afbc56
halityrd <halityrd@hotmail.com>
2025-08-14 03:36:40 +0300

    Styling Buttons

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -14,6 +14,10 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  void rollDice() {
+    // ...
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,7 +28,24 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(child: Image.asset('assets/images/dice-2.png', width: 200)),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/dice-2.png', width: 200),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28),
+              ),
+              child: const Text('Roll Dice'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
