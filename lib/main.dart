bde6dde2a571e9e45c11bb5b319ae58fdfd287d1
halityrd <halityrd@hotmail.com>
2025-08-14 03:04:51 +0300

    Reusable Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,14 @@ import 'package:dice_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+        ),
+      ),
+    ),
+  );
 }
